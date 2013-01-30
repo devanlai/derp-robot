bc7abbef17df57219b526826f41eaf1967720d47
Devan Lai <devan.lai@gmail.com>
2013-01-29 18:40:28 -0800

    Added initial hello world skeleton.

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <rfb/rfbclient.h>
+
+#include "robot.h"
+
+
+
+int main(int argc, char *argv[]) {
+	printf("Hello, world!\r\n");
+    return 0;
+}
---
+++ robot.h
@@ -0,0 +1,6 @@
+#ifndef __ROBOT_H__
+#define __ROBOT_H__
+
+
+
+#endif
